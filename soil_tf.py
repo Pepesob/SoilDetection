1707b4bb6ac45b077bfca663f0b96115ea5aca1b
Pepesob <pepesob2@gmail.com>
2024-03-10 21:01:35 +0100

    Initial commit

---
+++ soil_tf.py
@@ -0,0 +1,150 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+batch_size = 32
+img_height = 180
+img_width = 180
+data_dir = "./Soil_types"
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_ds.take(1):
+#     for i in range(9):
+#         ax = plt.subplot(3, 3, i + 1)
+#         plt.imshow(images[i].numpy().astype("uint8"))
+#         plt.title(class_names[labels[i]])
+#         plt.axis("off")
+#
+#
+# normalization_layer = layers.Rescaling(1./255)
+#
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# # Notice the pixel values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image))
+#
+# num_classes = len(class_names)
+#
+# data_augmentation = keras.Sequential(
+#   [
+#     layers.RandomFlip("horizontal",
+#                       input_shape=(img_height,
+#                                   img_width,
+#                                   3)),
+#     layers.RandomRotation(0.1),
+#     layers.RandomZoom(0.1),
+#   ]
+# )
+#
+# model = Sequential([
+#   data_augmentation,
+#   layers.Rescaling(1./255),
+#   layers.Conv2D(16, 3, padding='same', activation='relu'),
+#   layers.MaxPooling2D(),
+#   layers.Conv2D(32, 3, padding='same', activation='relu'),
+#   layers.MaxPooling2D(),
+#   layers.Conv2D(64, 3, padding='same', activation='relu'),
+#   layers.MaxPooling2D(),
+#   layers.Dropout(0.2),
+#   layers.Flatten(),
+#   layers.Dense(128, activation='relu'),
+#   layers.Dense(num_classes)
+# ])
+#
+# model.compile(optimizer='adam',
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#               metrics=['accuracy'])
+#
+# model.summary()
+#
+#
+# epochs=30
+# history = model.fit(
+#   train_ds,
+#   validation_data=val_ds,
+#   epochs=epochs
+# )
+#
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+#
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# epochs_range = range(epochs)
+#
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
+
+# model = tf.keras.models.load_model('saved_model/soil_recognition')
+#
+random_soil_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7LO84eN2OtroxV3CymZzVM9c3gWAoW-1K7Q&usqp=CAU"
+random_soil_path = tf.keras.utils.get_file('Soil1', origin=random_soil_url)
+#
+img = tf.keras.utils.load_img(
+    random_soil_path, target_size=(img_height, img_width)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+print(img_array)
+
+#
+# predictions = model.predict(img_array)
+# score = tf.nn.softmax(predictions[0])
+#
+# print(score)
+# print(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence."
+#     .format(class_names[np.argmax(score)], 100 * np.max(score))
+# )
+#
+# plt.imshow(img_array.numpy()[0].astype("uint8"))
+# plt.axis("off")
+# plt.show()
+
+# model.save('saved_model/soil_recognition')
+
+converter = tf.lite.TFLiteConverter.from_saved_model('saved_model/soil_recognition')
+tflite_model = converter.convert()
+
+# Save the model.
+with open('saved_model/soil_recognition_lite', 'wb') as f:
+    f.write(tflite_model)
